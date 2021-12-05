43b15caa4810947df29fd622441294544378353e
DW Rodrigues <denys.w.r@hotmail.com>
2021-12-05 15:00:43 -0300

    Commit final

---
+++ Locadora de veiculos/Controllers/VeiculosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Locadora_de_veiculos.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        // GET: Veiculos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
