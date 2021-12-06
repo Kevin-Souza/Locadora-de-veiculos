36d3003471a1c66dd5188f4ee7934b268d5d7df8
DW Rodrigues <denys.w.r@hotmail.com>
2021-12-05 22:20:57 -0300

    Criação da LocacaoController e Views

--- Locadora de veiculos/Controllers/LocacaoController.cs
+++ Locadora de veiculos/Controllers/LocacaoController.cs
@@ -16,7 +16,80 @@ namespace Locadora_de_veiculos.Controllers
             return View(context.Locacaos.OrderBy(c => c.Nome));
         }
 
+        // GET: Locacao/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
 
+        // GET: Locacao/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClienteID = new SelectList(context.Clientes.OrderBy(b => b.Nome),
+                "ClienteID", "Nome");
+            ViewBag.VeiculoID = new SelectList(context.Veiculos.OrderBy(b => b.Nome),
+                "VeiculoID", "Nome");
+            return View();
+        }
 
+        // POST: Locacao/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Locacao/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Locacao/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Locacao/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Locacao/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
